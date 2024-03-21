f7f4655ecccb81e04d1baa1671546986950c1c43
Aaron M. Graham <aaron.graham@veracitynuclear.com>
2024-03-20 23:01:23 -0400

    Strip enrico down to simply drive shift from main

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,34 @@
 #include "pugixml.hpp"
 #include <mpi.h>
 
-#include "enrico/coupled_driver.h"
-#include "enrico/mpi_types.h"
+#include "Nemesis/comm/Functions.hh"
+#include "Omnibus/driver/Multiphysics_Driver.hh"
+#include "Omnibus/driver/Sequence_Shift.hh" // for Sequence_Shift
+#include "Shift/mc_tallies/Cell_Union_Tally.hh"
+#include "Teuchos_DefaultMpiComm.hpp"          // for MpiComm
+#include "Teuchos_ParameterList.hpp"
+#include "Teuchos_XMLParameterListHelpers.hpp" // for RCP, ParameterList
+
+#include <unordered_map>
+
+struct Position {
+  // Constructors
+  Position() {}
+  Position(double x0, double y0, double z0)
+    : x{x0}
+    , y{y0}
+    , z{z0}
+  {}
+
+  double x{}; //!< x-coordinate
+  double y{}; //!< y-coordinate
+  double z{}; //!< z-coordinate
+};
 
 int main(int argc, char* argv[])
 {
   // Initialize MPI
   MPI_Init(&argc, &argv);
-  enrico::init_mpi_datatypes();
-
-  // Define enums for selecting drivers
-  enum class Transport { OpenMC, Shift, Surrogate };
 
   // Parse enrico.xml file
   pugi::xml_document doc;
@@ -25,33 +42,77 @@ int main(int argc, char* argv[])
   // Get root element
   auto root = doc.document_element();
 
-  // Determine transport driver
-  auto neut_driver = std::string{root.child("neutronics").child_value("driver")};
-  auto heat_driver = std::string{root.child("heat_fluids").child_value("driver")};
-  Transport driver_transport;
-  if (neut_driver == "openmc") {
-    driver_transport = Transport::OpenMC;
-  } else if (neut_driver == "shift") {
-    driver_transport = Transport::Shift;
-  } else if (neut_driver == "surrogate") {
-    driver_transport = Transport::Surrogate;
-  } else {
-    throw std::runtime_error{"Invalid value for <neutronics><driver>"};
-  }
-
   // Create driver according to selections
-  switch (driver_transport) {
-  case Transport::OpenMC:
-  case Transport::Shift: {
-    enrico::CoupledDriver driver{MPI_COMM_WORLD, root};
-    driver.execute();
-  } break;
-  case Transport::Surrogate:
-    throw std::runtime_error{"No surrogate particle transport driver implemented"};
-    break;
-  }
+  {
+    int max_timesteps_ = 1;
+    int max_picard_iter_ = 1;
+    auto node = root.child("neutronics");
+    // std::unique_ptr<enrico::ShiftDriver> neutronics_driver_ =
+    //     std::make_unique<enrico::ShiftDriver>(MPI_COMM_WORLD, node);
+    std::string filename = node.child_value("filename");
+    Teuchos::RCP<Teuchos::ParameterList> plist_ = Teuchos::RCP<Teuchos::ParameterList>(
+      new Teuchos::ParameterList("Omnibus_plist_root"));
+    plist_->set("input_path", filename);
+    auto teuchos_comm = Teuchos::MpiComm<int>(MPI_COMM_WORLD);
+    std::cout << "filename = " << filename << std::endl;
+    std::cout << "plist = " << std::endl;
+    plist_->print(std::cout);
+    std::cout << "comms = " << teuchos_comm << " " << MPI_COMM_WORLD << std::endl;
+    Teuchos::updateParametersFromXmlFileAndBroadcast(
+      filename, plist_.ptr(), teuchos_comm);
+    auto problem = std::make_shared<omnibus::Problem>(plist_);
+    std::shared_ptr<omnibus::Multiphysics_Driver> driver_ = std::make_shared<omnibus::Multiphysics_Driver>(problem);
+    std::shared_ptr<geometria::Geometry> geometry_ = problem->geometry();
+    // num_cells_ = geometry_->num_cells();
 
-  enrico::free_mpi_datatypes();
+    std::vector<geometria::Geometry::cell_type> cells_;
+    std::unordered_map<geometria::Geometry::cell_type, std::size_t> cell_index_;
+    std::vector<Position> positions;
+    positions.emplace_back(0.0, 0.0, 0.5);
+    positions.emplace_back(0.51, 0.0, 0.5);
+    positions.emplace_back(0.42, 0.0, 0.5);
+    positions.emplace_back(0.5, 0.0, 0.5);
+    // std::vector<std::size_t> elem_to_glob_cell_ = neutronics_driver_->find(positions);
+    std::vector<std::size_t> elem_to_glob_cell_;
+    elem_to_glob_cell_.reserve(positions.size());
+    for (const auto& r : positions) {
+      geometria::Space_Vector vec = {r.x, r.y, r.z};
+      geometria::Geometry::cell_type cell = geometry_->find_cell(vec);
+      if (cell_index_.find(cell) == cell_index_.end()) {
+        cell_index_[cell] = cells_.size();
+        cells_.push_back(cell);
+      }
+      auto h = cell_index_.at(cell);
+      elem_to_glob_cell_.push_back(h);
+    }
+
+    // neutronics_driver_->create_tallies();
+    auto tally_pl = Teuchos::sublist(plist_, "TALLY");
+    auto cell_pl = Teuchos::sublist(tally_pl, "CELL");
+    using Teuchos::Array;
+    auto power_pl = Teuchos::sublist(cell_pl, "power");
+    power_pl->set("name", "power");
+    power_pl->set("normalization", 1.0);
+    power_pl->set("description", std::string("power tally"));
+    Array<std::string> rxns(1, "fission");
+    power_pl->set("reactions", rxns);
+    power_pl->set("cycles", std::string("active"));
+    Array<std::string> cells(cells_.size());
+    for (int cellid = 0; cellid < cells_.size(); ++cellid){
+      cells[cellid] = geometry_->cell_to_label(cells_[cellid]);
+    }
+    Array<int> counts(cells_.size(), 1);
+    power_pl->set("union_cells", cells);
+    power_pl->set("union_lengths", counts);
+
+    for (int i_timestep_ = 0; i_timestep_ < max_timesteps_; ++(i_timestep_)){
+      for (int i_picard_ = 0; i_picard_ < max_picard_iter_; ++(i_picard_)){
+        driver_->rebuild();
+        driver_->run();
+      }
+    }
+  } // Make sure to deconstruct the driver before calling MPI_Finalize()
+
   MPI_Finalize();
   return 0;
 }
